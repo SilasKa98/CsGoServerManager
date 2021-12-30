03efc98523a2f6afb7e4d497dcc21b624623036d
SilasKa98 <92573936+SilasKa98@users.noreply.github.com>
2021-12-30 18:09:20 +0100

    Delete serverCommander directory

--- serverCommander/launchServer.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-    $server = $_POST["server"];
-    chdir('D:\csServerAconix\serverController\htdocs\Aconix\serverCommander\Servers\\'.$server);
-    exec('c:\WINDOWS\system32\cmd.exe /c START start.bat');
-?>
